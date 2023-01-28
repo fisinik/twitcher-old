f635e8ef1dcc483a49ec458cb450e1d3df3b5716
fisi <fisnik.spahijaa@gmail.com>
2023-01-28 17:34:22 +0100

    Montserrat and Ubuntu fonts with @next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,18 @@ import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import { Montserrat, Ubuntu } from '@next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+})
+
+const ubuntu = Ubuntu({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+  variable: '--font-ubuntu',
+})
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -11,7 +23,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <main className={`${montserrat.variable} font-sans ${ubuntu.variable} font-serif`}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 };
