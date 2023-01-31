1abd3f64a0007ab96bd78c58302803486ec22c4d
fisi <fisnik.spahijaa@gmail.com>
2023-01-31 17:37:25 +0100

    environment variables for uploading image

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -27,6 +27,9 @@ export const serverSchema = z.object({
  */
 export const clientSchema = z.object({
   // NEXT_PUBLIC_BAR: z.string(),
+  NEXT_PUBLIC_CLOUD_NAME: z.string(),
+  NEXT_PUBLIC_UPLOAD_PRESET: z.string(),
+  NEXT_PUBLIC_UPLOAD_URL: z.string(),
 });
 
 /**
@@ -37,4 +40,7 @@ export const clientSchema = z.object({
  */
 export const clientEnv = {
   // NEXT_PUBLIC_BAR: process.env.NEXT_PUBLIC_BAR,
+  NEXT_PUBLIC_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUD_NAME,
+  NEXT_PUBLIC_UPLOAD_PRESET: process.env.NEXT_PUBLIC_UPLOAD_PRESET,
+  NEXT_PUBLIC_UPLOAD_URL: process.env.NEXT_PUBLIC_UPLOAD_URL,
 };
