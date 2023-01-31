cd0624764863be3923af3b6b5c3df47840f2180e
fisi <fisnik.spahijaa@gmail.com>
2023-01-31 16:19:21 +0100

    fixed svg react errors

--- src/components/sighting-card/index.tsx
+++ src/components/sighting-card/index.tsx
@@ -22,20 +22,20 @@ export const SightingCard = ({ sighting }) => {
       </div>
       <div className="absolute left-0 right-0 bottom-4 px-3 flex justify-between">
         <a className="text-gray-800 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-          <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="#2DD4BF" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="#2DD4BF" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path d="M5 12h14"></path>
             <path d="M12 5l7 7-7 7"></path>
           </svg>
         </a>
         <div>
           <span className="text-gray-800 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-[1px] border-teal-400">
-            <svg className="w-4 h-4 mr-1" stroke="#2DD4BF" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+            <svg className="w-4 h-4 mr-1" stroke="#2DD4BF" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
               <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
               <circle cx="12" cy="12" r="3"></circle>
             </svg>1.2K
           </span>
           <span className="text-gray-800 inline-flex items-center leading-none text-sm">
-            <svg className="w-4 h-4 mr-1" stroke="#2DD4BF" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+            <svg className="w-4 h-4 mr-1" stroke="#2DD4BF" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
               <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
             </svg>6
           </span>
