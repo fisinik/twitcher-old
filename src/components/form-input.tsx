aeef81eaf8e057d6dae7bfac41cd1125ecaa3ab0
fisi <fisnik.spahijaa@gmail.com>
2023-02-10 19:54:24 +0100

    center input text

--- src/components/form-input.tsx
+++ src/components/form-input.tsx
@@ -38,7 +38,7 @@ export default function FormInput({
       <label
         htmlFor={`floating_${name}`}
         className="absolute text-sm text-gray-400 duration-300 transform -translate-y-4
-        scale-75 top-5 z-10 origin-[0] left-2.5 peer-placeholder-shown:scale-100 
+        scale-75 top-4 z-10 origin-[0] left-4 peer-placeholder-shown:scale-100 
         peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
       >
         {label}
