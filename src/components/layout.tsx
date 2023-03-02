8853a9967f9726aa7e2f23083adaab9703fdeeb2
fisi <fisnik.spahijaa@gmail.com>
2023-03-02 23:24:32 +0100

    add basic framer-motion animation for layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,20 @@
 import Header from "./header";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, y: 15 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 15 }}
+          transition={{ duration: 0.25 }}
+        >
+          <main>{children}</main>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
