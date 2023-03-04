6568f260f1f7035ea2db9a8658186395447cd346
fisi <fisnik.spahijaa@gmail.com>
2023-03-05 00:16:50 +0100

    loading skeleton color changes

--- src/components/bird-card/loading-skeleton.tsx
+++ src/components/bird-card/loading-skeleton.tsx
@@ -1,10 +1,10 @@
 export const LoadingSkeleton = () => {
   return (
     <div className="relative m-2 h-96 w-72 overflow-hidden rounded-md bg-white shadow-2xl">
-      <div className="h-72 w-full animate-pulse rounded-t-md rounded-b-[45px] bg-gray-300" />
+      <div className="h-72 w-full animate-pulse rounded-t-md rounded-b-[45px] bg-teal-200" />
       <div className="absolute left-0 right-0 bottom-5 z-10 flex flex-col items-center">
-        <h2 className="h-6 w-3/4 animate-pulse rounded-md bg-gray-300" />
-        <p className="mt-2 h-4 w-1/2 animate-pulse rounded-md bg-gray-300" />
+        <h2 className="h-6 w-3/4 animate-pulse rounded-md bg-teal-200" />
+        <p className="mt-2 h-4 w-1/2 animate-pulse rounded-md bg-teal-200" />
       </div>
     </div>
   );
--- src/components/sighting-card/loading-skeleton.tsx
+++ src/components/sighting-card/loading-skeleton.tsx
@@ -1,18 +1,19 @@
 export const LoadingSkeleton = () => {
   return (
     <div className="relative m-2 h-96 w-72 overflow-hidden rounded-md bg-white shadow-2xl">
+      <div className="h-60 w-full animate-pulse rounded-t-md bg-teal-200" />
       <div className="absolute left-0 right-0 top-64 z-10 flex items-center gap-x-4 px-3">
-        <div className="h-14 w-14 animate-pulse rounded-full bg-gray-300" />
+        <div className="h-14 w-14 animate-pulse rounded-full bg-teal-200" />
         <div className="flex flex-col">
-          <div className="h-4 w-32 animate-pulse bg-gray-300" />
-          <div className="h-3 w-24 animate-pulse bg-gray-300" />
+          <div className="h-4 w-32 animate-pulse bg-teal-200" />
+          <div className="h-3 w-24 animate-pulse bg-teal-200" />
         </div>
       </div>
       <div className="absolute left-0 right-0 bottom-4 flex justify-between px-3">
-        <div className="h-4 w-24 animate-pulse bg-gray-300" />
+        <div className="h-4 w-24 animate-pulse bg-teal-200" />
         <div className="flex gap-x-4">
-          <div className="h-4 w-12 animate-pulse bg-gray-300" />
-          <div className="h-4 w-12 animate-pulse bg-gray-300" />
+          <div className="h-4 w-12 animate-pulse bg-teal-200" />
+          <div className="h-4 w-12 animate-pulse bg-teal-200" />
         </div>
       </div>
     </div>
