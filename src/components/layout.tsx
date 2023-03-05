57c843d0018cce7299d92fc98b3f5a0259110d71
fisi <fisnik.spahijaa@gmail.com>
2023-03-05 01:38:18 +0100

    remove global animation

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,20 +1,10 @@
 import Header from "./header";
-import { motion, AnimatePresence } from "framer-motion";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <AnimatePresence>
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 50 }}
-          transition={{ duration: 0.4 }}
-        >
-          <main>{children}</main>
-        </motion.div>
-      </AnimatePresence>
+      <main>{children}</main>
     </>
   );
 }
