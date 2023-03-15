0c700d38b000a2a8956586fdfd7af333dc6f192c
fisinik <fisnik.spahijaa@gmail.com>
2023-03-15 11:30:58 +0100

    button animations

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 export default function Button({
   children,
   onClick,
@@ -9,12 +10,15 @@ export default function Button({
   className?: string;
 }) {
   return (
-    <button
+    <motion.button
       className={`mt-[15px] h-[50px] rounded-[3px]  bg-gradient-to-l from-teal-400 to-teal-500 text-white shadow-2xl hover:from-teal-500 hover:to-teal-600 ${className}`}
       onClick={onClick}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      transition={{ type: "spring", stiffness: 600, damping: 20 }}
       {...props}
     >
       {children}
-    </button>
+    </motion.button>
   );
 }
